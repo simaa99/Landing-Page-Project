e01deb37bda0c712100813df88e8591e6ff325f4
simaa99 <semaamosa99@gmail.com>
2023-07-08 22:18:31 +0300

    create special card component

---
+++ src/components/SpecialCardComponent/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./style.css";
+export default class index extends Component {
+  render() {
+    const { imageSrc, imageAlt, cardTitle, cardDescription } = this.props;
+    return (
+      <div className="card">
+        <img src={imageSrc} alt={imageAlt} className="card-icon" />
+        <div class="card-content">
+          <h2 className="card-title">{cardTitle}</h2>
+          <p className="card-description">{cardDescription}</p>
+        </div>
+      </div>
+    );
+  }
+}
