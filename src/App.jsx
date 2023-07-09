3654e7c15ee0c3adcba89d34854a62e7b111548e
simaa99 <semaamosa99@gmail.com>
2023-07-10 01:53:42 +0300

    add statistic section to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import SpecialSection from "./Sections/SpecialSection";
 import BlogSection from "./Sections/BlogSection";
 import ReviewsSection from "./Sections/ReviewsSection";
 import MenuSection from "./Sections/MenuSection";
+import StatisticSection from "./Sections/StatisticSection";
 
 export default class App extends Component {
   render() {
     return (
       <>
         <SpecialSection />
-        <MenuSection/>
-        <ReviewsSection/>
+        <MenuSection />
+        <StatisticSection />
+        <ReviewsSection />
         <BlogSection />
       </>
     );
