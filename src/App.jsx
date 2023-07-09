eb419fd7bfee14f8b3875600eefddb8d025abe0b
simaa99 <semaamosa99@gmail.com>
2023-07-09 23:53:04 +0300

    add special section to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import "./App.css"
+import SpecialSection from './Sections/SpecialSection'
 
 export default class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <SpecialSection/>
     )
   }
 }
