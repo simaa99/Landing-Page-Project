3dbdd7c72d8377d5cc291a83569493b68361c14b
simaa99 <semaamosa99@gmail.com>
2023-07-10 00:33:43 +0300

    add blog data

---
+++ src/mock/BlogData.js
@@ -0,0 +1,35 @@
+import Icon1 from '../assests/BlogImg/1.svg';
+import Icon2 from '../assests/BlogImg/2.svg';
+import Icon3 from '../assests/BlogImg/3.svg';
+
+const BlogData = [
+  {
+    id: "1",
+    blogTitle: "Cooking Dining Experience",
+    blogImg: Icon1,
+    blogDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin ",
+    blogButton: "Read More",
+    isReverse :"false"
+  },
+  {
+    id: "2",
+    blogTitle: "Cooking Dining Experience",
+    blogImg: Icon2,
+    blogDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin ",
+    blogButton: "Read More",
+    isReverse :"true"
+
+  },
+  {
+    id: "3",
+    blogTitle: "Cooking Dining Experience",
+    blogImg: Icon3,
+    blogDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin ",
+    blogButton: "Read More",
+    isReverse :"false"
+
+  }
+];
+
+export default BlogData;
+
