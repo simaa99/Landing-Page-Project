7d8483350706fbd4812677b683b519488752dd75
simaa99 <semaamosa99@gmail.com>
2023-07-10 01:13:36 +0300

    add review data

---
+++ src/mock/ReviewData.js
@@ -0,0 +1,28 @@
+import Icon1 from "../assests/ReviewImg/1.svg";
+import Icon2 from "../assests/ReviewImg/2.svg";
+
+const ReviewData = [
+  {
+    id: "1",
+    personName: "Alex andrina",
+    personImg: Icon1,
+    descReviews:
+      "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras. Quisque commodo mattis ornare a nec odio. Pulvinar orci, egestas eget nulla duis at iaculis nunc, elit. “",
+  },
+  {
+    id: "2",
+    personName: "Alex andrina",
+    personImg: Icon1,
+    descReviews:
+      "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras. Quisque commodo mattis ornare a nec odio. Pulvinar orci, egestas eget nulla duis at iaculis nunc, elit. “",
+  },
+  {
+    id: "3",
+    personName: "Alex andrina",
+    personImg: Icon2,
+    descReviews:
+      "“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras. Quisque commodo mattis ornare a nec odio. Pulvinar orci, egestas eget nulla duis at iaculis nunc, elit. “",
+  },
+];
+
+export default ReviewData;
