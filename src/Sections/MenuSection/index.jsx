4503befc31606599062e1282d65a08783e6eff5e
simaa99 <semaamosa99@gmail.com>
2023-07-09 12:09:52 +0300

    create menu component section

---
+++ src/Sections/MenuSection/index.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import MenuCardComponent from '../../components/MenuCardComponent'
+import Container from '../../components/ContainerComponent'
+import menuData from '../../mock/menuData'
+import TitleComponent from '../../components/TitleComponent'
+import './style.css'
+export default class index extends Component {
+  render() {
+    return (
+        <Container width={80}> 
+         <TitleComponent
+         title="Straight From Kitchen"
+        subtitle="Our Menu"
+        isCenter={true}
+        isReverse={false}
+      />
+        <div className='menu-item'>    
+        {menuData.map((item) => (
+          <MenuCardComponent
+            key={item.id}
+            titleCard={item.titleCard}
+            descriptionCard={item.descriptionCard}
+            price={item.price}
+            imageCardMenu={item.imageCardMenu}
+            imageAltCard="Card Image"
+          />
+        ))}
+        </div>   
+       </Container>
+    )
+  }
+}
