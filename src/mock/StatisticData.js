8e94173fb2fc244cb8742583f3ecbbf036bfbd88
simaa99 <semaamosa99@gmail.com>
2023-07-10 01:52:43 +0300

    add Statistic dtat

---
+++ src/mock/StatisticData.js
@@ -0,0 +1,30 @@
+import Icon1 from '../assests/StatisticImg/1.svg';
+import Icon2 from '../assests/StatisticImg/2.svg';
+import Icon3 from '../assests/StatisticImg/3.svg';
+
+const StatisticData = [
+  {
+    id: "1",
+    numberStatistic: "250",
+    imageStatistic: Icon1,
+    descStatistic: "Delicacy",
+    imageAltStatistic: "icon 1",
+  },
+  {
+    id: "2",
+    numberStatistic: "10",
+    imageStatistic: Icon2,
+    descStatistic: "renowed chefs",
+    imageAltStatistic: "icon 2",
+  },
+  {
+    id: "3",
+    numberStatistic: "30",
+    imageStatistic: Icon3,
+    descStatistic: "Members",
+    imageAltStatistic: "icon 3",
+  }
+];
+
+export default StatisticData;
+
