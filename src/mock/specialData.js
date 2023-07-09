be1a32d04b20e0e5434f8ae918967d13fa7df9e7
simaa99 <semaamosa99@gmail.com>
2023-07-09 23:52:38 +0300

    add data of specail section

---
+++ src/mock/specialData.js
@@ -0,0 +1,30 @@
+import Icon1 from '../assests/specialIcon/1.svg';
+import Icon2 from '../assests/specialIcon/2.svg';
+import Icon3 from '../assests/specialIcon/3.svg';
+
+const specialData = [
+  {
+    id: "1",
+    cardTitle: "Fresh food",
+    imageSrc: Icon1,
+    cardDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras. ",
+    imageAlt: "icon 1",
+  },
+  {
+    id: "2",
+    cardTitle: "skilled Chef",
+    imageSrc: Icon2,
+    cardDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras. ",
+    imageAlt: "icon 2",
+  },
+  {
+    id: "3",
+    cardTitle: "Exotic dishes",
+    imageSrc: Icon3,
+    cardDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin hendrerit a amet viverra. Nunc pretium in amet at mattis cras. ",
+    imageAlt: "icon 3",
+  }
+];
+
+export default specialData;
+
