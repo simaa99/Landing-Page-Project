e3d986ee01b745720ed93255557a0bf8991d77db
simaa99 <semaamosa99@gmail.com>
2023-07-10 01:14:53 +0300

    add menu section & review section to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import SpecialSection from "./Sections/SpecialSection";
 import BlogSection from "./Sections/BlogSection";
+import ReviewsSection from "./Sections/ReviewsSection";
+import MenuSection from "./Sections/MenuSection";
 
 export default class App extends Component {
   render() {
     return (
       <>
         <SpecialSection />
+        <MenuSection/>
+        <ReviewsSection/>
         <BlogSection />
       </>
     );
