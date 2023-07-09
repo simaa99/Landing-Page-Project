440afdcdbc4ac31beb78a61dce5fe832a1689f93
simaa99 <semaamosa99@gmail.com>
2023-07-09 12:09:23 +0300

    add data fo menu component

---
+++ src/mock/menuData.js
@@ -0,0 +1,102 @@
+import Icon1 from '../assests/menuIcon/1.svg';
+import Icon2 from '../assests/menuIcon/2.svg';
+import Icon3 from '../assests/menuIcon/3.svg';
+import Icon4 from '../assests/menuIcon/4.svg';
+import Icon5 from '../assests/menuIcon/5.svg';
+import Icon6 from '../assests/menuIcon/6.svg';
+import Icon7 from '../assests/menuIcon/7.svg';
+import Icon8 from '../assests/menuIcon/8.svg';
+import Icon9 from '../assests/menuIcon/9.svg';
+import Icon10 from '../assests/menuIcon/10.svg';
+import Icon11 from '../assests/menuIcon/11.svg';
+import Icon12 from '../assests/menuIcon/12.svg';
+
+const menuData = [
+  {
+    id: "1",
+    titleCard: "Lorem ipsum dolor sit amet",
+    imageCardMenu: Icon1,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 222",
+  },
+  {
+    id: "2",
+    titleCard: "Vivamus sodales augue vita",
+    imageCardMenu: Icon2,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 590",
+  },
+  {
+    id: "3",
+    titleCard: "Pellentesque semper semper",
+    imageCardMenu: Icon3,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 130",
+  },
+  {
+    id: "4",
+    titleCard: "Vestibulum est turpis",
+    imageCardMenu: Icon4,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 135",
+  },
+  {
+    id: "5",
+    titleCard: "Phasellus convallis maximus",
+    imageCardMenu: Icon5,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 148",
+  },
+  {
+    id: "6",
+    titleCard: "Nullam elementum magna",
+    imageCardMenu: Icon6,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 200",
+  },
+  {
+    id: "7",
+    titleCard: "Phasellus convallis maximus",
+    imageCardMenu: Icon7,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 148",
+  },
+  {
+    id: "8",
+    titleCard: "Vestibulum est turpis",
+    imageCardMenu: Icon8,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 135",
+  },
+  {
+    id: "9",
+    titleCard: "Vivamus sodales augue vita",
+    imageCardMenu: Icon9,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 590",
+  },
+  {
+    id: "10",
+    titleCard: "Lorem ipsum dolor sit amet",
+    imageCardMenu: Icon10,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 222",
+  },
+  {
+    id: "11",
+    titleCard: "Nullam elementum magna",
+    imageCardMenu: Icon11,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 200",
+  },
+  {
+    id: "12",
+    titleCard: "Pellentesque semper semper",
+    imageCardMenu: Icon12,
+    descriptionCard: "Lorem ipsum dolor sit amet",
+    price: "Rs 130",
+  },
+];
+
+export default menuData;
+
