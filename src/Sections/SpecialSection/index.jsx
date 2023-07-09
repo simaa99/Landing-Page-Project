5c88d29fe923f52d8f71cae433185bdc9dd67dfc
simaa99 <semaamosa99@gmail.com>
2023-07-09 23:52:18 +0300

    add special section

---
+++ src/Sections/SpecialSection/index.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import SpecialCardComponent from "../../components/SpecialCardComponent";
+import Container from "../../components/ContainerComponent";
+import TitleComponent from "../../components/TitleComponent";
+import "./style.css";
+import specialData from "../../mock/specialData";
+export default class index extends Component {
+  render() {
+    return (
+      <div className="specail-section">
+        <Container width={90}>
+          <TitleComponent
+            title="Special"
+            subtitle="What makes us special"
+            isCenter={true}
+            isReverse={false}
+          />
+          <div class="special-item">
+            {specialData.map((item) => (
+              <SpecialCardComponent
+                key={item.id}
+                cardTitle={item.cardTitle}
+                cardDescription={item.cardDescription}
+                imageSrc={item.imageSrc}
+                imageAlt={item.imageAlt}
+              />
+            ))}
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
