1b09dcf58c7cd12eff1f3d0e748ae3ed59df5852
simaa99 <semaamosa99@gmail.com>
2023-07-09 12:08:37 +0300

    edit title component

--- src/components/TitleComponent/index.jsx
+++ src/components/TitleComponent/index.jsx
@@ -1,19 +1,44 @@
 import React, { Component } from "react";
-import "./style.css"
-export default class index extends Component {
-  render() {
-    const {title , subtitle , isCenter} = this.props;
+import "./style.css";
 
-    const titleColor = isCenter ? "#FFFFFC" : "#FFCE6D";
-    const subtitleColor = isCenter ? "#FFCE6D" : "#FFFFFC";
-    const alignItems = isCenter ? "center" : "initial";
+export default class Index extends Component {
+  getStyles = () => {
+    const { isCenter, isReverse } = this.props;
+    let titleColor, subtitleColor, alignItems;
+
+    if (isCenter) {
+      titleColor = isReverse ? "#FFFFFC" : "#FFCE6D";
+      subtitleColor = isReverse ? "#FFCE6D" : "#FFFFFC";
+      alignItems = "center";
+    } else {
+      titleColor = "#FFCE6D";
+      subtitleColor = "#FFFFFC";
+      alignItems = "initial";
+    }
+
     const rowGap = isCenter ? "1rem" : "0rem";
+
+    return {
+      alignItems,
+      rowGap,
+      titleColor,
+      subtitleColor,
+    };
+  };
+
+  render() {
+    const { title, subtitle } = this.props;
+    const styles = this.getStyles();
+
     return (
-        <div className="title-section" style={{ alignItems , rowGap }}>
-        <h2 className="title" style={{ color: titleColor }}>
+      <div
+        className="title-section"
+        style={{ alignItems: styles.alignItems, rowGap: styles.rowGap }}
+      >
+        <h2 className="title" style={{ color: styles.titleColor }}>
           {title}
         </h2>
-        <p className="subtitle" style={{ color: subtitleColor }}>
+        <p className="subtitle" style={{ color: styles.subtitleColor }}>
           {subtitle}
         </p>
       </div>
