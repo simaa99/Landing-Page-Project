d89ec47da4c7749725138160d3326377bb677479
simaa99 <semaamosa99@gmail.com>
2023-07-10 13:44:33 +0300

    add cooking & about sections to app

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import BlogSection from "./Sections/BlogSection";
 import ReviewsSection from "./Sections/ReviewsSection";
 import MenuSection from "./Sections/MenuSection";
 import StatisticSection from "./Sections/StatisticSection";
+import AboutSection from "./Sections/AboutSection";
+import CookingSection from "./Sections/CookingSection";
 
 export default class App extends Component {
   render() {
     return (
       <>
+        <AboutSection />
         <SpecialSection />
         <MenuSection />
+        <CookingSection/>
         <StatisticSection />
         <ReviewsSection />
         <BlogSection />
