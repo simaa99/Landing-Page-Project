275eb5c16e3ed2fe2c38430e9ad9b69dec5cdfee
simaa99 <semaamosa99@gmail.com>
2023-07-11 13:11:57 +0300

    edit blog data

--- src/mock/BlogData.js
+++ src/mock/BlogData.js
@@ -1,35 +1,33 @@
-import Icon1 from '../assests/BlogImg/1.svg';
-import Icon2 from '../assests/BlogImg/2.svg';
-import Icon3 from '../assests/BlogImg/3.svg';
+import Icon1 from "../assests/BlogImg/1.svg";
+import Icon2 from "../assests/BlogImg/2.svg";
+import Icon3 from "../assests/BlogImg/3.svg";
 
 const BlogData = [
   {
     id: "1",
     blogTitle: "Cooking Dining Experience",
     blogImg: Icon1,
-    blogDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin ",
+    blogDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin ",
     blogButton: "Read More",
-    isReverse :"false"
   },
   {
     id: "2",
     blogTitle: "Cooking Dining Experience",
     blogImg: Icon2,
-    blogDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin ",
+    blogDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin ",
     blogButton: "Read More",
-    isReverse :"true"
-
+    isReverse: "true",
   },
   {
     id: "3",
     blogTitle: "Cooking Dining Experience",
     blogImg: Icon3,
-    blogDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin ",
+    blogDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sollicitudin ",
     blogButton: "Read More",
-    isReverse :"false"
-
-  }
+  },
 ];
 
 export default BlogData;
-
