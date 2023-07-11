e1728c237ceada043ee364ca1f00486c91a12728
simaa99 <semaamosa99@gmail.com>
2023-07-11 13:11:34 +0300

    create footer data

---
+++ src/mock/footerData.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import icon1 from "../assests/footerImg/1.svg";
+import icon2 from "../assests/footerImg/2.svg";
+import icon3 from "../assests/footerImg/3.svg";
+
+const footerData = [
+  {
+    srcImg: icon1,
+    text: "Gogreendineservice@gmail.com",
+    alt: "icon1",
+  },
+  {
+    srcImg: icon2,
+    text: "AZ complex bylane3 Mandari Rd Mumbai 1100867",
+    alt: "icon2",
+  },
+  {
+    srcImg: icon3,
+    text: "+1800 287 256",
+    alt: "icon3",
+  },
+];
+
+export default footerData;
