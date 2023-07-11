46daaa96002e69014fea63cc25d39196ba620aed
simaa99 <semaamosa99@gmail.com>
2023-07-11 13:10:36 +0300

    create footer list component

---
+++ src/components/FooterListComponent/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./style.css";
+export default class index extends Component {
+  render() {
+    return (
+      <div className="footer-list">
+        <span>Other Links</span>
+        <ul>
+          <li>• Privacy Policy</li>
+          <li>• Terms & conditions</li>
+          <li>• Blogs</li>
+          <li>• our team</li>
+          <li>• Our kitchen</li>
+        </ul>
+      </div>
+    );
+  }
+}
